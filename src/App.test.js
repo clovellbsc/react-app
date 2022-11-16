8bd75d05141206b86e00ddbb96ab410f5ef2bfb5
Chris Lovell <clovellbsc@gmail.com>
2022-11-16 16:14:40 +0000

    added testing for url link

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test("renders learn react link", () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("correct URL on react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement.href).toContain("reactjs.org");
+});
