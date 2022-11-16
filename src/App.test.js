296c187f38814d6364b8bf0e7ed31ae70ea045b4
Chris Lovell <clovellbsc@gmail.com>
2022-11-16 15:40:41 +0000

    testing CI with failing test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+test("renders text", () => {
+  render(<App />);
+  const linkElement = screen.getByText("Edit src/App.js and save to reload");
+  expect(linkElement).not.toBeInTheDocument();
 });
